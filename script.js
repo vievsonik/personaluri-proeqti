e2bcc3c5022e354bfcd480f27313c3f47b58a335
vievsonik <118842811+vievsonik@users.noreply.github.com>
2023-03-02 23:32:51 +0400

    best changes

--- script.js
+++ script.js
@@ -5,36 +5,32 @@ const  weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 const innerBox = document.querySelector('.inner-box');
 
-
+// 728b0ee6df5687559812bd3169ad77b7
 search.addEventListener('click', () =>{
-    
-    const APIKey = '728b0ee6df5687559812bd3169ad77b7';
+  
+    const APIKey = '5360a0e22e268d8f063bdd4b4e954ccf';
     const city = document.querySelector('.search-box input').value;
     if (city === '')
         return
-
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
         .then(response => response.json())
         .then(json => {
         if(json.cod === '404'){
-            container.style.height = '404px';
-            weatherBox.style.disply = 'none';
-            weatherDetails.style.disply = 'none';
-            error404.style.disply = 'block';
+            innerBox.style.height = '570px';
+            weatherBox.style.display = 'none';
+            weatherDetails.style.display = 'none';
+            error404.style.display = 'block';
             error404.classList.add('fadeIn');
             return;
         }
-
+    
         error404.style.disply = 'none';
           error404.classList.remove('fadeIn');
 
           const image = document.querySelector('.weather-box img');
           const temperature = document.querySelector('.weather-box .temperature');
-          const discription = document.querySelector('.weather-box .description');
-          const humidity = document.querySelector('.weather-details .humidity span')
-          const wind = document.querySelector('.weather-details .wind span')
-          
-          switch (json.weather[0].main) {
+          const discription = document.querySelector('.weather-box .description');        
+          switch (json.weather[0].main){
             case 'Clear':
                 image.src = 'images/clear.png';
                 break;
@@ -63,9 +59,7 @@ search.addEventListener('click', () =>{
 
           temperature.innerHTML = `${parseInt(json.main.temp)}<span> *C </span>`;
           discription.innerHTML = `${json.weather[0].description}`;
-          humidity.innerHTML = `${(json.main.humidity)}%`;
-          wind.innerHTML = `${parseInt(json.wind.speed)} km/h `;
-
+        
           weatherBox.style.disply = '';
           weatherDetails.style.disply = '';
           weatherBox.classList.add('fadeIn');
